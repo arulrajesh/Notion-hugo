beec42d8cb63bb0ce758a3b6009e91ceee8dd38c
Arul Rajesh <arul.rajesh@gmail.com>
2023-12-07 15:29:44 +0530

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/fpvindia/Notion-DoIt-0a6ad331b7e5483db3309c7e5e31dba6',
         pages: [
             // {
             //     page_id: '<page_id>',
